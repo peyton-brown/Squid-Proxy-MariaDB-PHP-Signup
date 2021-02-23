02464b34ef00543d933d6c69481584b68909515e
peyton-brown <peytonedwardbrown@gmail.com>
2021-02-23 09:50:21 -0500

    Adding MySQL Support

--- MySQLFiles/MySQLCode.sql
+++ MySQLFiles/MySQLCode.sql
@@ -3,13 +3,13 @@ CREATE DATABASE squid;
 USE squid;
 
 CREATE TABLE `users` (
-  `user` VARCHAR(32) NOT NULL PRIMARY KEY default '',
+  `username` VARCHAR(32) NOT NULL PRIMARY KEY default '',
   `password` VARCHAR(32) NOT NULL default '',
   `fullname` VARCHAR(64) default NULL
 );
 
 One line version:
-	CREATE TABLE `users` (`user` VARCHAR(32) NOT NULL PRIMARY KEY default "", `password` VARCHAR(32) NOT NULL default "", `fullname` VARCHAR(64) default NULL);
+	CREATE TABLE `users` (`username` VARCHAR(32) NOT NULL PRIMARY KEY default "", `password` VARCHAR(32) NOT NULL default "", `fullname` VARCHAR(64) default NULL);
 
 INSERT INTO users VALUES('rootUser', 'rootPWD', 'Root User');
 
