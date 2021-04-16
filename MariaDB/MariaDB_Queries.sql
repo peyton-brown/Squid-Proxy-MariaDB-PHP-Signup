6954e1a33ac97e05a74b9ad58c6192cdf48deaf7
Peyton Brown <peyton.brown@intra.qsibanking.com>
2021-04-16 11:52:38 -0400

    Added sql user and granted privileges

--- MariaDB/MariaDB_Queries.sql
+++ MariaDB/MariaDB_Queries.sql
@@ -1,18 +1,22 @@
+CREATE USER 'squiduser'@'localhost' IDENTIFIED BY 'squidpwd';
+
 CREATE DATABASE squid;
 
 USE squid;
 
-CREATE TABLE `users` (
-  `username` VARCHAR(32) NOT NULL PRIMARY KEY default '',
-  `password` VARCHAR(32) NOT NULL default '',
-  `fullname` VARCHAR(64) default NULL
+CREATE TABLE 'users' (
+  'id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  'username' VARCHAR(32) NOT NULL default '',
+  'password' VARCHAR(32) NOT NULL default '',
+  'fullname' VARCHAR(64) default NULL
 );
 
-One line version:
-	CREATE TABLE `users` (`username` VARCHAR(32) NOT NULL PRIMARY KEY default "", `password` VARCHAR(32) NOT NULL default "", `fullname` VARCHAR(64) default NULL);
+CREATE TABLE 'users' ('id' INT NOT NULL AUTO_INCREMENT, 'username' VARCHAR(32) NOT NULL PRIMARY KEY default '', 'password' VARCHAR(32) NOT NULL default '', 'fullname' VARCHAR(64) default NULL);
 
 INSERT INTO users VALUES('rootUser', 'rootPWD', 'Root User');
 
 SHOW TABLES;
 
 SELECT * FROM users;
+
+GRANT ALL PRIVILEGES ON squid.* TO 'squiduser'@'localhost';
