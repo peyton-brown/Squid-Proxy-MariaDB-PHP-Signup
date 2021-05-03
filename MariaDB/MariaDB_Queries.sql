f387ebf884bb416751ccdaa072b8a80f08bd9e89
Peyton Brown <peyton.brown@intra.qsibanking.com>
2021-05-03 11:17:10 -0400

    Updated users table

--- MariaDB/MariaDB_Queries.sql
+++ MariaDB/MariaDB_Queries.sql
@@ -4,16 +4,14 @@ CREATE DATABASE squid;
 
 USE squid;
 
-CREATE TABLE 'users' (
-  'id' INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  'username' VARCHAR(32) NOT NULL default '',
-  'password' VARCHAR(32) NOT NULL default '',
-  'fullname' VARCHAR(64) default NULL
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username VARCHAR(32) NOT NULL default '',
+  password VARCHAR(32) NOT NULL default '',
+  fullname VARCHAR(64) default NULL
 );
 
-CREATE TABLE 'users' ('id' INT NOT NULL AUTO_INCREMENT, 'username' VARCHAR(32) NOT NULL PRIMARY KEY default '', 'password' VARCHAR(32) NOT NULL default '', 'fullname' VARCHAR(64) default NULL);
-
-INSERT INTO users VALUES('rootUser', 'rootPWD', 'Root User');
+INSERT INTO users(username, password, fullname) VALUES ('testUser', 'testPWD', 'Test User');
 
 SHOW TABLES;
 
