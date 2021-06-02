d5a83dc2d51b06395d2a6e6683a779d2fffa7a4d
Peyton Brown <peyton.brown@intra.qsibanking.com>
2021-06-02 08:28:08 -0400

    Updated table information

--- MariaDB/MariaDB_Queries.sql
+++ MariaDB/MariaDB_Queries.sql
@@ -1,15 +1,19 @@
 CREATE DATABASE squid;
 
+GRANT SELECT ON squid.* to dataproxy@localhost identified by 'dataproxy';
+
 USE squid;
 
 CREATE TABLE passwd (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  username VARCHAR(32) NOT NULL default '',
-  password VARCHAR(32) NOT NULL default '',
-  fullname VARCHAR(64) default NULL
+    user varchar(32) NOT NULL default '',
+    password varchar(35) NOT NULL default'',
+    enabled tinyint(1) NOT NULL default '1',
+    fullname varchar(60) default NULL,
+    comment varchar(60) default NULL,
+    PRIMARY KEY (user)
 );
 
-INSERT INTO passwd(username, password, fullname) VALUES ('testUser', 'testPWD', 'Test User');
+INSERT INTO passwd VALUES('testUser','testPWD',1,'Test User','comment test');
 
 SHOW TABLES;
 
