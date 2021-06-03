171bb24cf95f416d4875a0dcc80500652ec5de27
Peyton Brown <peyton.brown@intra.qsibanking.com>
2021-06-03 09:31:55 -0400

    simplified table data

--- MariaDB/MariaDB_Queries.sql
+++ MariaDB/MariaDB_Queries.sql
@@ -7,13 +7,10 @@ USE squid;
 CREATE TABLE passwd (
     user varchar(32) NOT NULL default '',
     password varchar(35) NOT NULL default'',
-    enabled tinyint(1) NOT NULL default '1',
-    fullname varchar(60) default NULL,
-    comment varchar(60) default NULL,
     PRIMARY KEY (user)
 );
 
-INSERT INTO passwd VALUES('testUser','testPWD',1,'Test User','comment test');
+INSERT INTO passwd VALUES('testUser','testPWD');
 
 SHOW TABLES;
 
