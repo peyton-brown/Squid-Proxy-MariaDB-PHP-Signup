9be5706099122cf1177b851476d0838e2a7fb1b8
peyton-brown <peytonedwardbrown@gmail.com>
2021-06-23 18:08:23 -0400

    Added enabled variable back in for testing

--- MariaDB/make-db.sql
+++ MariaDB/make-db.sql
@@ -8,10 +8,11 @@ USE squid;
 CREATE TABLE passwd (
     user varchar(32) NOT NULL default '',
     password varchar(35) NOT NULL default '',
+    enabled tinyint(1) NOT NULL default '1',
     PRIMARY KEY (user)
 );
 
-INSERT INTO passwd VALUES('testUser','testPWD');
+INSERT INTO passwd VALUES('testUser','testPWD',1);
 
 SHOW TABLES;
 
--- PHP-Signup-Website/inc/signup-include.php
+++ PHP-Signup-Website/inc/signup-include.php
@@ -12,7 +12,7 @@
         }
 
         //TO INSERT username and password from field to jossyusers database
-        $query = "INSERT INTO passwd(user,password) VALUES('$username','$password')";
+        $query = "INSERT INTO passwd(user,password,enabled) VALUES('$username','$password',1)";
         $result = mysqli_query($conn, $query);
         if(!$result){
             die("Query failed ".mysqli_error($conn));
