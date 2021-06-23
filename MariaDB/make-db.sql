2278edb8dcea9b9fa8396a3c28772ae2b99f7ed3
peyton-brown <peytonedwardbrown@gmail.com>
2021-06-23 13:58:06 -0400

    Changed MySQL user to squidUser instead of dataproxy

--- MariaDB/make-db.sql
+++ MariaDB/make-db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE squid;
 
-GRANT SELECT ON squid.* to dataproxy@localhost identified by 'dataproxy';
+GRANT SELECT ON squid.* to squidUser@localhost identified by 'squidPassword';
 
 USE squid;
 
